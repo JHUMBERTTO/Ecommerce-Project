841d10b6282a2bfcd2bb4caddcf807cfe08ad0bb
Jhumberto <jhumbertto@gmail.com>
2023-05-11 10:33:46 -0600

    Navbar Update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,40 +3,54 @@ import {TbSearch} from "react-icons/tb"
 import {FiShoppingBag} from "react-icons/fi"
 import {BsBookmarkHeart} from "react-icons/bs"
 import React, { useEffect, useState } from 'react'
+import ShopBag from '../../Cart/ShopBag/ShopBag'
+import { useNavigate} from "react-router-dom"
+import SearchBar from '../SearchBar/SearchBar'
 
 const Navbar = () => {
+  const navigate = useNavigate();
   const[scrolled, setScrolled ] = useState(false)
+  const[showShopBag, setShowShopBag ] = useState(false)
+  const[showSearchBar, setShowSearchBar ] = useState(false)
   const handleScroll = () => {
     const offset = window.scrollY;
-    if(offset> 200){
+    if(offset> 180){
       setScrolled(true)
     }else {
       setScrolled(false)
     }
   }
 
+
   useEffect(( ) => {
     window.addEventListener("scroll", handleScroll)
   }, [])
+  
   return (
+  <>  
     <nav className={`mainNav ${scrolled ? 'stickyNav' : ""}`}>
       <div className="navContent">
         <ul className="left">
-          <li className="hac">Home</li>
-          <li className="hac">About</li>
-          <li className="hac">Categories</li>
+          <li className="hac" onClick={()=>{navigate("/") }}>Home</li>
         </ul>
-        <div className="center">JHGL</div>
+        <div className="center" onClick={()=>{navigate("/") }}>JHGL</div>
         <div className="right">
-          <TbSearch/>
+          <TbSearch onClick={() =>
+            {setShowSearchBar(true)}
+          }/>
           <BsBookmarkHeart/>
-          <span className='icon'>
+          <span className='icon' onClick={() =>
+            {setShowShopBag(true)}
+          }>
             <FiShoppingBag/>
             <span className='counter'>5</span>
           </span>
         </div>
       </div>
     </nav>
+    {showShopBag && <ShopBag setShowShopBag={setShowShopBag}/>}
+    {showSearchBar && <SearchBar setShowSearchBar={setShowSearchBar}/>}
+  </>
   )
 }
 
